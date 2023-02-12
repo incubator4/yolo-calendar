e902015ff81d4aafb5cb54d7fa9163f08378e044
incubator4 <aries0robin@gmail.com>
2023-02-12 23:41:18 +0800

    update main color

--- pkg/table.go
+++ pkg/table.go
@@ -20,9 +20,10 @@ type Character struct {
 
 type CharacterCalendar struct {
 	Calendar
-	Name   string `json:"name"`
-	UID    int    `json:"uid" gorm:"column:uid"`
-	LiveID int    `json:"live_id" gorm:"column:live_id"`
+	Name      string `json:"name"`
+	UID       int    `json:"uid" gorm:"column:uid"`
+	LiveID    int    `json:"live_id" gorm:"column:live_id"`
+	MainColor string `json:"main_color" gorm:"column:main_color"`
 }
 
 func (CharacterCalendar) TableName() string {
