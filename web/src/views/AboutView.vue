08db44a8355fa74dc23570c626aca4c7203f62d0
incubator4 <aries0robin@gmail.com>
2023-02-12 23:03:05 +0800

    update about page

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>关于该项目</h1>
+    <h3>
+      只是个人用爱发电做的直播日历汇总，后续可能会有新的功能增加<br />
+      如果您有更好的建议或者意见<br />
+      可以通过
+      <el-link
+        href="https://github.com/incubator4/yolo-calendar"
+        target="_blank"
+        >Github</el-link
+      >
+      或者 QQ:337376577 联系我
+    </h3>
   </div>
 </template>
 
